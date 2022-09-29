fd6ef469ed84760fef243086cdcb1ef4e0c8de84
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-09-29 11:34:53 +0000

    trying single quotes

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,8 +4,8 @@
  * */
 
 #include <stdio.h>
-int main(void)
+int main()
 {
-	puts('Programming is like building a multilingual puzzle);
+	puts("\'Programming is like building a multilingual puzzle");
 	return (0);
 }
