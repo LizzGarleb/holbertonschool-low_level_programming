3ba6a8cab699033a516a82ea9aea95f6c88d442f
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-09-29 12:15:30 +0000

    task 5 betty documentatio

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+int main(void)
+	/**
+	 * main - print string 
+	 *
+	 * Description: Write a C program that print a quote follow by a new line. 
+	 *
+	 * Return: Always 0 (Success)
+	 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
