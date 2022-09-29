6556a1dbedb709a45c68bb392418ff4ef86cd153
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-09-29 11:20:24 +0000

    task 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,8 @@
+/* Write a C program that prints exactly "Programming is like buidling a multilingual puzzle" followed by a new line. */
+
+#include <stdio.h>
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
