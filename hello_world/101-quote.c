0d31a759646bd46c5bf05ac59a649a0da35cd572
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-09-29 12:42:33 +0000

    task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	/*
+	 * Main - prints a string
+	 * Return: return 1 (Pass)
+	 *
+	 */
+	putchar("and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
