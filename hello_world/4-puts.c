b4496ab957b253aeb67cc66264ea9f20e8270f57
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-09-29 11:59:27 +0000

    betty documentation part 6

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-/**
+/*
  * main - Prints string
  * 
  * Description: Write a C program that prints exactly "Proramming is like building a multilingual puzzle", followed by a new line.
  *
  * Return: Always 0 (Success)
- **/
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
