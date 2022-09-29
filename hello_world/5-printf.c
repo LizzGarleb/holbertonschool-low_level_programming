f7fd7c933c11805c6ae3b1b75afc568ac95b6761
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-09-29 13:39:12 +0000

    task 5 betty proof

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
+/**
+* main - print string
+* Return: Always 0 (Success)
+*/
 int main(void)
-	/**
-	 * main - print string
-	 * Return: Always 0 (Success)
-	 */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
