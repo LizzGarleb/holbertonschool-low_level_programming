fcea3e1864f4c0731e36d2f49f483a50750e8907
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-09-29 13:34:59 +0000

    task 8-101 betty proof

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+/**
+* main - prints a string
+* Return: return 1 (Pass)
+*
+*/
+
 int main(void)
 {
-	/*
-	 * Main - prints a string
-	 * Return: return 1 (Pass)
-	 *
-	 */
-	write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 62);
+	write(2, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 9);
 	return (1);
 }
