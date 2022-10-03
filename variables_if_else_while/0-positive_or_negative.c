b0e5f11d6758ddb9f1e1312e2d1270135bceff0b
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-03 13:06:50 +0000

    task 0 output correction

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if (n < 0)
+	if (n > 0)
 		printf("%i is positive\n", n);
 	if (n == 0)
 		printf("%i is zero\n", n);
-	if (n > 0) 
+	if (n < 0) 
 		printf("%i is negative\n", n);
 	return (0);
 }
