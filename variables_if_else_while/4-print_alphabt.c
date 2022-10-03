1e2d5f717aea4267c5a4115de70d4210a6cbf319
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-03 23:55:14 +0000

    task 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints a string
+ * Return: return (0) Passed
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if ( ch == e ; ch == q)
+			continue
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print a string
+ * Return: return 0 success
+ */
+int main(void)
+{
+	printf( '\n')
+	return 0;
+}
