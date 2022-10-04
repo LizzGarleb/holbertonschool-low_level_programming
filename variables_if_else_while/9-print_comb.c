09169d8cda95d9e110a03c69591f2427511d14e7
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-04 13:57:17 +0000

    task 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - print a string
+ * Retun: return 0 sucessfull
+ */
+int main(void)
+{
+	int corn;
+
+	for (corn = '0' ; corn <= '9' ; corn++)
+	{
+		putchar(corn);
+
+		if (corn != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
