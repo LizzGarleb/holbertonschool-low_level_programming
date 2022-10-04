b702b182ad9c39c77a2ee363028d597e0e19350c
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-04 01:15:19 +0000

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print a string
+ * Return: return 0 sucess
+ */
+int main(void)
+{
+	int war;
+
+	for (war = '0'; war <= '9'; war++)
+	{
+		putchar(war);
+	}
+	putchar('\n');
+	return (0);
+}
