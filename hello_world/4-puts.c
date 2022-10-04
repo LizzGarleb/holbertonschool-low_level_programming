70b64f4bcd5106d099bbc586bd7644a2807b9bf3
Lizbeth García Lebrón <lizbethgarcialebron@gmail.com>
2022-10-04 17:50:00 -0400

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Prints "Programming is like building a multilingual puzzle".
- * Return: Return is void (no return).
+ * Return: Return 0 when done.
  */
 int main(void)
 {
