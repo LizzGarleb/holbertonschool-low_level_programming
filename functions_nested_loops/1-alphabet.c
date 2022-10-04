d34d64e5c2bfc717a37192897ba2ad79f90a0953
Lizbeth García Lebrón <lizbethgarcialebron@gmail.com>
2022-10-04 18:39:01 -0400

    Update 1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * print_alphabet - Write a function that prints the alphabet, in lowercase, followed by a new line.
- * Return: Return 0 when done.
+ * Return: Theres no return.
  */
 void print_alphabet(void)
 {
