af661b77b41151a2486a7925ac832ca0cf24b92c
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-04 18:47:54 +0000

    task 4 part 2

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -7,7 +7,11 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'Z'))
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c >= 'Z')
 	{
 		return (1);
 	}
