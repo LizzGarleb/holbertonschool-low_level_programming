1f90de33c1677e25be9e3b5914365fe98389424d
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-04 17:55:23 +0000

    task 2 part 7

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,9 +8,9 @@ void print_alphabet_x10(void)
 	char ch;
 	int num;
 
-	for (num = 0; num <= 10; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (ch = 'a'; ch <= 'z'; ch++;'\n')
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}		
