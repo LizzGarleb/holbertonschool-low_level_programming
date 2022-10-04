ba21508ecb75cd42a75f9bbcd89f0ec1da6616da
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-04 22:55:46 +0000

    task 5

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,3 +5,21 @@
  *
  * Return: zero is passed
  */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else 
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
