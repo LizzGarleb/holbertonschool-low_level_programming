15187a55ac914174bc55a287f6f02ff7b37f6616
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-04 00:05:19 +0000

    task 4 part 2

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,8 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		if ( ch == e ; ch == q)
-			continue
-		putchar(ch);
+		if (ch != 'e' && ch != 'q')
+			putchar(ch);
 	}
 	putchar('\n');
 	return (0);
