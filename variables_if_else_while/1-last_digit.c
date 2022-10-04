cf997dd3be3467b7aa23a5b44e15e618a114e905
Lizbeth García Lebrón <lizbethgarcialebron@gmail.com>
2022-10-04 17:47:48 -0400

    Update 1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 /**
 * main - The program will assign a random number to the variable n each time it is executed.
 * 	Print the last digit of the number stored in the variable n.
-* Return: Return void (no return).
+* Return: Return 0 when done.
 */
 int main(void)
 {
