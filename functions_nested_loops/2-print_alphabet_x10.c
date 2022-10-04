1a3fa57ad62c187a01a2dd76e1cf927dc14b222e
Lizbeth García Lebrón <lizbethgarcialebron@gmail.com>
2022-10-04 18:33:52 -0400

    Update 2-print_alphabet_x10.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - print alhphabet
- * Return: return void
+ * print_alphabet_x10 - Print 10 times the alphabet, in lowercase, followed by a new line.
+ * Return: Return 0 when done.
  */
 void print_alphabet_x10(void)
 {
