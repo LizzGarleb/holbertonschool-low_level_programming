cda48de8fd3aa8511cf5cf85c797a451ded69a56
Lizbeth García Lebrón <lizbethgarcialebron@gmail.com>
2022-10-04 17:45:05 -0400

    Update 8-print_base16.c

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Print all the numbers of base 16 in lowercase, followed by a new line.
- * Return: Return void (no return).
+ * Return: Return 0 when done.
  */
 int main(void)
 {
