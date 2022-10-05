1994be8b77a1bc00570be5d712d3d9612a53275b
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-05 23:20:30 +0000

    checking

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,9 +1,31 @@
 #include "main.h"
 /**
- *
+ * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
+ * @n: Its a random number.
+ * Return: Number count from n to 98.
  */
 void print_to_98(int n)
 {
+	int a, b;
 
-	_putchar('\n')
+	if(n <= 98)
+	{
+		for(a = n; a <= 98; a++)
+		{	
+		_putchar(a);
+		_putchar(',');
+		_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	else if (n >= 98)
+	{
+		for(b = n; b >= 98; b--)
+		{
+			_putchar(b);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
 }
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * time_tables - Prints 9 times table, starting with 0.
+ * Return: 9 times table in block format.
+ */
+void times_table(void)
+{
+        int x, y, z, z1, z2;
+
+        for (x = 0 ; x <= 9 ; x++)
+        {
+                for (y = 0 ; y <= 9 ; y++)
+                {
+
+                z = x * y;
+                z1 = ((z / 10) + '0');
+                z2 = ((z % 10) + '0');
+
+                if (z1 > 48)
+                {
+                _putchar(z1);
+                }
+                
+		if (y != 8)
+                {
+	 	 _putchar(z2);
+        	 _putchar(',');
+        	 _putchar(' ');
+         	 _putchar(' ');
+                }
+		else
+		{
+			_putchar(z2);
+		}
+		}
+	_putchar('\n');
+        }
+}
