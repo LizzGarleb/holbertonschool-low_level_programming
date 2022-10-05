703ff1560d5443d94c588c0f2a17b4d74e1e870c
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-05 23:59:10 +0000

    task 9 part 8

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
  * @n: Its a random number.
@@ -6,26 +7,20 @@
  */
 void print_to_98(int n)
 {
-	int a, b;
-
 	if(n <= 98)
 	{
-		for(a = n; a <= 98; a++)
-		{	
-		_putchar(a);
-		_putchar(',');
-		_putchar(' ');
+		while (n <= 98; n++)
+		{
+		printf("%i, ", n);
 		}
-		_putchar('\n');
+		printf('\n');
 	}
 	else if (n >= 98)
 	{
-		for(b = n; b >= 98; b--)
+		while (n >= 98; n--)
 		{
-			_putchar(b);
-			_putchar(',');
-			_putchar(' ');
+			printf("%i, ", n);
 		}
-		_putchar('\n');
+		printf('\n');
 	}
 }
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,34 +5,36 @@
  */
 void times_table(void)
 {
-        int x, y, z, z1, z2;
+	int x, y, z, firstdigit, lastdigit;
 
-        for (x = 0 ; x <= 9 ; x++)
-        {
-                for (y = 0 ; y <= 9 ; y++)
-                {
+	for (x = 0; x <= 9; x++)
+	{
 
-                z = x * y;
-                z1 = ((z / 10) + '0');
-                z2 = ((z % 10) + '0');
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			firstdigit = ((z / 10) + '0');
+			lastdigit = ((z % 10) + '0');
 
-                if (z1 > 48)
-                {
-                _putchar(z1);
-                }
-                
-                if (y != 8)
-                {
-                 _putchar(z2);
-                 _putchar(',');
-                _putchar(' ');
-                 _putchar(' ');
-                }
-                else
-                {
-                     _putchar(z2);
-                }
-                }
-        _putchar('\n');
-        }
+		if (z <= 9 && y != 0)
+		{
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(firstdigit);
+		}
+
+		if (y != 0)
+		{
+			_putchar(',');
+		}
+		if (y <= 8)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+		}
+	_putchar('\n');
+	}
 }
