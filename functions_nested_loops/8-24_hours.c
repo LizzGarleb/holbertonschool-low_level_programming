580963af4efe6068d6d4f5e35db7452ddc5718e1
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-05 12:14:14 +0000

    task 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * jack_bauer - 
+ * Return: Return void (no return).
+ */
+void jack_bauer(void)
+{	
+	int a
+	
+	if (a = "00:00"; a >= "23:59"; a++)
+	{
+		_putchar('a')
+	}
+}
