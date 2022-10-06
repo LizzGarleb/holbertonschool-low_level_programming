3063dcdec07df9ddb41b28bcc55d722182408175
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-06 00:29:57 +0000

    betty bullet proof task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,40 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_to_98 - Prints all natural numbers from n to 98, followed by a new line.
+ * print_to_98 - Prints all natural numbers from n to 98
  * @n: Its a random number.
  * Return: Number count from n to 98.
  */
 void print_to_98(int n)
 {
-	if(n <= 98)
+	if (n <= 98)
 	{
-		while (n <= 98; n++)
+		while (n <= 98)
 		{
-		printf("%i, ", n);
+			if (n == 98)
+			{
+				printf("%i\n", n);
+			}
+			else
+			{
+				pritf("%i, ", n)
+			}
+			n++;
 		}
-		printf('\n');
 	}
-	else if (n >= 98)
+	else
 	{
-		while (n >= 98; n--)
+		while (n >= 98)
 		{
-			printf("%i, ", n);
+			if (n == 98)
+			{
+				printf("%i\n", n);
+			}
+			else
+			{
+				printf("%i, ", n);
+			}
+			n--;
 		}
-		printf('\n');
 	}
 }
