c1fad97012be71cce5b115553ae20dcd2b486fa2
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-06 00:41:55 +0000

    task 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - adds two interger
+ * @a: They are random numbers
+ * @b: They are random numbers
+ * Return: the result.
+ */
+int add(int a, int b)
+{
+	int a
+	int b
+
+	printf(a + b);
+}
