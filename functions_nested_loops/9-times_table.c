39c0687c298ea11fde5ca05bb0c191f11c32ccbe
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-06 00:01:22 +0000

    betty documentation correction task 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * time_table - Prints 9 times table, starting with 0.
+ * times_table - Prints 9 times table, starting with 0.
  * Return: 9 times table in block format.
  */
 void times_table(void)
