7e31968a4e0e9e49b6ca3efec3bb0ed48ab32680
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-06 23:43:52 +0000

    task 3 correcting

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers, from 0 to 9.
+ * Return: Printed number and a new line.
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n');
+}
