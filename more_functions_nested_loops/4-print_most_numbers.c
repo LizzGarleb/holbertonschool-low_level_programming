c7752a5a83a3a14a74b6e981a7a5b41d45c6675a
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-07 00:17:22 +0000

    task 5

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints numbers from 0 to 9.
+ */
+void print_most_numbers(void)
+{
+	int a
+	
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(a + '0');
+	}
+	_putchar('\n')
+}
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times the numbers.
+ * Return: Numbers from 0 to 14 followed by a new line.
+ */
+void more_numbers(void)
+{
+	int a = 0;
+	int b;
+
+	for (a = 0; a <= 10; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+	_putchar('\n');
+	}
+}
