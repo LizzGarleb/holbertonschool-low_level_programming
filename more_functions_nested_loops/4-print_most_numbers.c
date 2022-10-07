cf29f8d77a9f7698501dd4fd88927a92f4fc728b
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-07 00:43:58 +0000

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -4,11 +4,14 @@
  */
 void print_most_numbers(void)
 {
-	int a
+	int a;
 	
 	for (a = 0; a <= 9; a++)
 	{
+		if (a != 2 && a != 4)
+		{
 		_putchar(a + '0');
+		}
 	}
-	_putchar('\n')
+	_putchar('\n');
 }
--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,16 +5,16 @@
  */
 void more_numbers(void)
 {
-	int a = 0;
+	int a;
 	int b;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b <= 14; b++)
 		{
 			_putchar(b / 10 + '0');
 			_putchar(b % 10 + '0');
 		}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
