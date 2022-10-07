a40431060c3f00ade1eff437a693ea7ab63b6613
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-07 12:07:40 +0000

    task 7 lol

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -13,9 +13,9 @@ void print_diagonal(int n)
 	{
 		int a, b;
 
-		for (a = 0; a <= n; a++)
+		for (a = 0; a < n; a++)
 		{
-			for (b = 0; b < a; b++)
+			for (b = 0; b < n; b++)
 			{
 				if (b == a)
 				{
