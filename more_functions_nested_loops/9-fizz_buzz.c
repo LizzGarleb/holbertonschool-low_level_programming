75a7a125a47db7396f671654e2b9d3777626aa9e
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-07 12:51:40 +0000

    betty correction task 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,12 +9,12 @@ int main(void)
 
 	for (a = 1; a <= 100; a++)
 	{
-		if (a%15 == 0)
-			printf ("FizzBuzz ");
-		else if ((a%3) == 0)
-			printf ("Fizz ");
-		else if ((a%5) == 0)
-			printf ("Buzz ");
+		if (a % 15 == 0)
+			printf("FizzBuzz ");
+		else if ((a % 3) == 0)
+			printf("Fizz ");
+		else if ((a % 5) == 0)
+			printf("Buzz ");
 		else
 			printf("%d ", a);
 	}
