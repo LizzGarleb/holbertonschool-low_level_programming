993997488568c803cc7e9d3be807151d0d2f5421
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-09 18:41:38 +0000

    task 12

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Print all possible different combination of three digits
+ * Return: return 0
+ */
+int main(void)
+{
+	int num, num2;
+
+	for (num = 0; num <= 98; num++)
+	{
+		for (num2 = num + 1; num2 <= 99; num2++)
+		{
+			putchar(num / 10 + '0');
+			putchar(num % 10 + '0');
+			putchar(' ');
+			putchar(num2 / 10 + '0');
+			putchar(num2 % 10 + '0');
+			if (num == 98 && num2 == 99)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
