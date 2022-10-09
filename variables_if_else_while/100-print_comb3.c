66d4ed6528aa4b5e7c150865f5fb88a104f57cd3
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-09 16:52:22 +0000

    task 10 accidental typo

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -21,34 +21,7 @@ int main(void)
 			{
 				putchar(num);
 				putchar(num2);
-				putchar(',');int main(void)
-{
-        int num, num2;
-        for (num =  48; num <= 56; num ++)
-        {
-                for (num2 = 49; num2 <= 57; num2++)
-                {
-
-                        if (num == 56 && num2 == 57)
-                        {
-                                putchar(num);
-                                putchar(num2);
-                                putchar(' ');
-                        }
-                        else
-                        {
-                                putchar(num);
-                                putchar(num2);
-                                putchar(',');
-                                putchar(' ');
-                        }
-                }
-        }
-        putchar('\n');
-        return (0);
-}
-
-				putchar(' ');
+				putchar(',');
 			}
 		}
 	}
