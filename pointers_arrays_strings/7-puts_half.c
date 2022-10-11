aeeb1a12d4c6fa8819f37f7546d3e43f0d303e24
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-11 14:48:18 +0000

    task 7 checker

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: giving string pointer
+ */
+void puts_half(char *str)
+{
+	int lenght, even, odd;
+
+	while (str[lenght] != '\0')
+	{
+		lenght ++;
+	}
+	if (lenght % 2 == 0)
+	{
+		for (even = lenght / 2; str[even] != '\0'; even++)
+		{
+			_putchar(str[even]);
+		}
+	} 
+	else if (lenght % 2)
+	{
+		for (odd = (lenght - 1) / 2; odd < lenght - 1; odd ++)
+		{
+		       _putchar(str[odd + 1]);
+		}
+	}
+	_putchar('\n');
+}
