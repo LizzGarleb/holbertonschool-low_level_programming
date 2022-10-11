95879676e7351c35f57fb1ad2ec6820724ae8520
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-11 20:42:17 +0000

    tas 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -14,3 +14,4 @@ char *_strcpy(char *dest, char *src);
 /*c - more pointers, arrays and strings */
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
