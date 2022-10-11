bed205199a2d2ee38ce89ece7510a3783b256fe9
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-11 13:51:50 +0000

    task 6

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - Print every other character of a string
+ * @str: given point string
+ */
+void puts2(char *str)
+{
+	int lenght, a;
+
+	lenght = 0;
+
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+	
+	for (a = 0; a < lenght; a++)
+	{
+		if ((a % 2) == 0)
+		{
+		_putchar(str[a]);
+		}
+	}
+	_putchar('\n');
+}
