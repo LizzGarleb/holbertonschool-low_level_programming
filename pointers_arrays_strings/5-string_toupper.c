2cbd2e6a504dd4e312d390bbfa8e519c219e938e
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-12 00:18:04 +0000

    task 5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *
+ */
+char *string_toupper(char *str)
+{
+	int lenght, convert;
+
+	lenght = 0;
+
+	while (str[lenght] != '\0')
+	{
+		lenght++;
+	}
+
+	for (convert = 0; convert < lenght; convert++)
+	{
+		if (str[convert] >= 'a' && str[convert] <= 'z')
+		{
+			str[convert] = str[convert] - 32;
+		}
+	}
+	return (str);
+}
