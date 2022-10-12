d9073aa89161f9218fa2f5a381947d0a80a9367e
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-12 19:17:11 +0000

    task 7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encode a string into 1337
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char let[10] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+
+	char num[10] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == let[j])
+			{
+				s[i] = num[j];
+			}
+		}
+	}
+	return (s);
+}
