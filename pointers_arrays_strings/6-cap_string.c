e0d046bc2f1b55715bc427346af8c01de341d582
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-12 14:01:42 +0000

    task 6 completed, betty proof

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,27 +2,29 @@
 /**
  * cap_string - capitalizes all word of a string
  * @str: string
+ * Return: return the value of str
  */
 char *cap_string(char *str)
 {
 	int i, n;
-	
-	char comp[13]= {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
-	
+
+	char comp[13] = {' ', '\t', '\n', ',', ';', '.',
+'!', '?', '"', '(', ')', '{', '}'};
+
 	for (i = 0 ; str[i] != '\0'; i++)
 	{
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 		{
-				str[i] -= 32;
+			str[i] -= 32;
 		}
 		for (n = 0; n < 13; n++)
 		{
 			if (str[i] == comp[n])
 			{
-				if (str[i] >= 'a' && str[i] <= 'z')
-                        	{
-                           	     str[i + 1] -= 32;
-                        	}
+				if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+				{
+					str[i + 1] -= 32;
+				}
 			}
 		}
 	}
