1ea5e66803473b4d031a433941357ad220a3c6c5
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-13 17:36:56 +0000

    task 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,15 +4,14 @@
  * ocurrence of the character c in the string s.
  * @c: specifies the character to be search for
  * @s: specifies the pointer to the null termianted string
+ * Return: the value of string if truth otherwise 0
  */
 char *_strchr(char *s, char c)
 {
-	do
-	{
+	do{
 		if (*s == c)
 			return (s);
-	}
-	while (*s++);
+	} while (*s++);
 
 	return (0);
 }
