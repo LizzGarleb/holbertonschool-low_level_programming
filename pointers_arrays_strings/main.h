0a86f27031182f16aeabff05e24029d4036ea715
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-13 17:31:08 +0000

    task 2

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,3 +24,4 @@ char *leet(char *);
 /*c - even more pointers, arrays and strigns */
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
