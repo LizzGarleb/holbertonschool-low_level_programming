22908873bc0ab2d4e0be22fb26985d950979c08e
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-13 23:35:07 +0000

    task 3 betty

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -9,20 +9,20 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	int i, j;
-		
+
 	for (i = 0; s[i]; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
-                        {
-                                break;
-                        }
-                }
+			{
+				break;
+			}
+		}
 		if (s[i] != accept[j])
-                {
+		{
 			break;
-                }
+		}
 	}
 	return (i);
 }
