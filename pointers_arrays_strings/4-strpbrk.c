302391b925be53627b0b3469e9f7e7366e07ad8c
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-14 01:02:26 +0000

    task 4 betty

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -3,6 +3,8 @@
  * _strpbrk - search a string for any of a set of bytes
  * @s: string to compared
  * @accept: string that we gonna use
+ * Return: a pointer to the byte in s that matches one
+ * of the bytes in accept, or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
