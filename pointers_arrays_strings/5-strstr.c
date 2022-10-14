5ca498ccd155e2a09417bd4c514581ccc5c5ffd6
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-14 13:32:36 +0000

    task 5 pushing to github

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,9 @@
 #include "main.h"
 /**
- *
+ * _strstr - locate a substring
+ * @haystack: main string
+ * @needle: substring
+ * Return: the value of needle and haystack
  */
 char *_strstr(char *haystack, char *needle)
 {
@@ -12,7 +15,26 @@ char *_strstr(char *haystack, char *needle)
 		{
 			if (i == j)
 			{
-				return (needle);
+				return (haystack);
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+	return (0);
+}char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i]; i++)
+	{
+		for (j = 0; needle[j]; j++)
+		{
+			if (i == j)
+			{
+				return (haystack);
 			}
 			else
 			{
