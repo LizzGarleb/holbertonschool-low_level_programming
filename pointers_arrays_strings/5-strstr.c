760840795a4d60e306f6fc3d094dd701fbec6ac0
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-14 17:27:16 +0000

    task 4 backup

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,13 +9,13 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; haystack[i]; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j]; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
 			if (haystack[i] == needle[j])
 			{
-				return (haystack[i]);
+				return (haystack[i + j]);
 			}
 			else
 			{
