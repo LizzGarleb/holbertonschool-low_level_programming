31488181836586716b3214d72a4bc7a0eec28c8b
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-15 13:06:20 +0000

    task 7

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -12,8 +12,8 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		sum1 += (*a + (i * size + i));
-		sum2 += (*a + (i * size + (size - i - 1)));
+		sum1 += *(a + (i * size + i));
+		sum2 += *(a + (i * size + (size - i - 1)));
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
