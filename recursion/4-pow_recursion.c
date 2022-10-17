4d9766b1407e4cf256e45d54f0ea5a74f6062371
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-17 18:42:51 +0000

    task 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -6,5 +6,20 @@
  */
 int _pow_recursion(int x, int y)
 {
-	if 
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1);
+	}
+	else if (x == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
 }
