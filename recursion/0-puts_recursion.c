b308fa99f044b306651e0dec06c78742e8a1fcc0
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-17 14:09:30 +0000

    recusion

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -5,20 +5,16 @@
  */
 void _puts_recursion(char *s)
 {
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+	}
+	
 	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 
-	_putchar(*s);
-	s++;
-
-	if (*s != '\0')
-	{
-		_puts_recursion(s);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+	_puts_recursion(s);
 }
