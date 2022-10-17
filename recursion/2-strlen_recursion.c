2f5062a0b15d39f8ad7ffff9a21bcd43bd9ed107
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-17 15:00:32 +0000

    task 2 betty proof

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,15 +1,18 @@
 #include "main.h"
 /**
- *
+ * _strlen_recursion - return the lenght of a string
+ * @s: string to find lenght
+ * Return: return 0 if it's equal to null byte, otherwise
+ * return string lenght.
  */
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return 0;
+		return (0);
 	}
 	else
 	{
-		return 1 + _strlen_recursion(s + 1);
+		return (1 + _strlen_recursion(s + 1));
 	}
 }
