04ae331035978542cc50cdf7d5acbcc699079082
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-17 18:44:03 +0000

    task 4 betty

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -3,6 +3,8 @@
  * _pow_recursion - return the value of x raised
  * to the power of y.
  * Return: if y is lower then 0 return -1.
+ * @x: base
+ * @y: power
  */
 int _pow_recursion(int x, int y)
 {
