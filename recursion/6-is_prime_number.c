d802e7bae459c74c08c8b31e170629a257a1e287
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-18 12:36:45 +0000

    task 7

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+int prime(int n, int i);
+/**
+ * is_prime_number - find prime number
+ * @n: given number
+ */
+int is_prime_number(int n)
+{
+	return (prime(n, 2));
+}
+
+/**
+ * prime - calculates de prime
+ * @n: given number
+ * @i: does the work
+ * Return: 1 if n is prime, 0 if not
+ */
+int prime(int i, int n)
+{
+	if (n == 0 || n == 1)
+	{
+		return (0);
+	}
+	if (n == i)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (prime(i + 1, n));
+}
