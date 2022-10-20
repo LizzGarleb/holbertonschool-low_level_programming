026a2682e6d8eb1e153d85c0f33151842a3af9f7
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-20 12:45:25 +0000

    task 4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that adds positive numbers
+ * @argc: count how many arguments are
+ * @argv: holds the arguments
+ * Return: return 0 if passed otherwise return 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	int i, a;
+
+	a = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+
+		if (*argv[i] == 101)
+		{
+			printf("Error\n");
+			return(1);
+		}
+	}
+	printf("%d\n", a);
+	return (0);
+}
