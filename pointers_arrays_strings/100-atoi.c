e7d9e3706ca7366b0c816db02dcf95144f38f81c
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-21 21:08:34 +0000

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,27 +1,56 @@
-#includ "main.h"
+#include "main.h"
 /**
- * _atoi - convers a string to an interger.
- * @s: string to be converted
+ * _atoi - Convert a string to an integer
+ * @s: string to convert
+ * Return: return r if negative, return sum if positive.
  */
 int _atoi(char *s)
 {
-	int i, j, n, c;
+	int i, j, n, convert, mul, sum, r;
 
-	char *d;
+	char *d, *a;
+
+	n = 0;
+	mul = 1;
+	convert = 0;
+	sum = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if (s[i] == '-')
+		{
+			n++;
+		}
 		if (s[i] > 47 && s[i] < 58)
 		{
 			d = s + i;
 			break;
 		}
-		if (s[i] == '-');
+	}
+
+	for (j = 0; d[j] != '\0'; j++)
+	{
+		if (d[j] < 48 || d[j] > 57)
 		{
-			n++;
+			d[j] = '\0';
+			break;
 		}
 	}
-	c = s[i] - 48;
+	a = d + (j - 1);
 
-	
+	while (a != d - 1)
+	{
+		convert = *a - 48;
+		sum += convert * mul;
+		mul = mul * 10;
+		a--;
+	}
+
+	if (!n % 2 == 0)
+	{
+		r = sum * -1;
+		return (r);
+	}
+
+	return (sum);
 }
