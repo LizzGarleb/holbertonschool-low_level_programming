91aebbd00bd43952e4aa95c19a8363862de5acbe
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-21 20:37:13 +0000

    testing

--- pointers_arrays_strings/test.c
+++ pointers_arrays_strings/test.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 int main()
 {
-	int i, j, n, c, m, t;
+	int i, j, n, convert, mul, sum, r;
 	
 	char *d, *a;
 
-	char s[]="hel,-----+567jovnao ----6969s";
+	char s[]="hel,++---567jovnao ----6969s";
 
 	n = 0;
-	m = 1;
-	c = 0;
-	t = 0;
+	mul = 1;
+	convert = 0;
+	sum = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
+		if(s[i] == '-')
+		{
+			n++;
+		}
+
 		if (s[i] > 47 && s[i] < 58)
 		{
 			d = s + i;
@@ -34,11 +39,17 @@ int main()
 
 	while (a != d - 1)
 	{
-		c = *a - 48;
-		t += c * m;
-		m = m * 10;
+		convert = *a - 48;
+		sum += convert * mul;
+		mul = mul * 10;
 		a--;
 	}
-	printf("%d\n", c);
+
+	if (!n % 2 == 0)
+	{
+		r = sum * -1;
+	}
+
+	printf("%d\n", r);
 	return (0);
 }
