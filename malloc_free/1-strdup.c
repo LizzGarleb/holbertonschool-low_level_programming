3af17ac7902a48179b3dc8b95aceed145b46b2b3
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-24 15:46:29 +0000

    task 1 terminal proof

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - Function that returns a pointer to a newly allocated
+ * space in memory, which contains a copy of the string given as
+ * a parameter.
+ * @str: string to be copy
+ * Return: return null if str = null otherwise return value of p
+ */
+char *_strdup(char *str)
+{
+	int lenght;
+	char *p, *temp;
+
+	lenght = strlen(str);
+
+	p = malloc(sizeof(char) * lenght + 1);
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	temp = p;
+	while (*str)
+	{
+		*temp = *str;
+		temp++;
+		str++;
+	}
+
+	temp = '\0';
+
+	return (p);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -1,2 +1,4 @@
 int _putchar(char c);
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+
