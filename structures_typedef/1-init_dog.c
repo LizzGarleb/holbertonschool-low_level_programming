8d5ea91591b96594bbf0b7e5d2e1c11590350dca
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-31 19:10:39 +0000

    task 0

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,8 @@
+/**
+ * init_dog - write a function that initialize a
+ * variable of type struct dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog = {&d, *name, age, *owner};
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog {
-  char *name;
-  float age;
-  char *owner;
-}
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog information
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
