e792b2e831170b22c02e1299f07c5546df099745
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-10-31 21:01:29 +0000

    correction of struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -19,12 +19,12 @@ char *owner;
  * @age: dog age
  * @owner: dog owner
  */
-struct dog_T
+struct dog
 {
 char *name;
 float age;
 char *owner;
-};
+} dog_T;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
