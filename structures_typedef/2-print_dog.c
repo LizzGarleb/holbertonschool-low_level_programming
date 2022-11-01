cf9b01c0d7acb46b7d0f7656f726e7050f7f55a2
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-01 12:32:48 +0000

    task 2 spaces

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,14 +11,14 @@ void print_dog(struct dog *d)
 	}
 	else if (d->name == NULL || d->owner == NULL)
 	{
-		printf("Name:(nil)\nAge:%f\nOwner:(nil)\n", d->age);
+		printf("Name: (nil)\nAge: %f\nOwner: (nil)\n", d->age);
 	}
 	else if (d->age < 0)
 	{
-		printf("Name:%s\nAge:(nil)\nOwner:%s\n", d->name, d->owner);
+		printf("Name: %s\nAge: (nil)\nOwner: %s\n", d->name, d->owner);
 	}
 	else
 	{
-		printf("Name:%s\nAge:%f\nOwner:%s\n", d->name, d->age, d->owner);
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 	}
 }
