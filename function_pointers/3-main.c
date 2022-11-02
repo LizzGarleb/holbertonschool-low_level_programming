4d0979828acdcaddc49b83addec2c1d03c6299c1
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-02 03:31:20 +0000

    betty correction

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,6 +13,7 @@ int main(int argc, char *argv[])
 	int one, two, a;
 	int (*p)(int, int);
 	char *ope;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -21,8 +22,8 @@ int main(int argc, char *argv[])
 	one = atoi(argv[1]);
 	two = atoi(argv[3]);
 	ope = argv[2];
-	if ((argv[2][0] != '*' && argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' && 
-				argv[2][0] != '%') || argv[2][1] != '\0')
+	if ((argv[2][0] != '*' && argv[2][0] != '+' && argv[2][0] != '-' &&
+argv[2][0] != '/' && argv[2][0] != '%') || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
