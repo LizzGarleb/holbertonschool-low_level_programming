f8601e912841d93b6093d46a4a6685acd210fe71
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-02 03:00:45 +0000

    task 3 hope

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -6,18 +6,21 @@
  * function to persom the operation asked by the user.
  *
  * @s: operator passed as argument to the program
+ *
+ * Return: NULL if does not match
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i = 0;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	int i = 0;
 
 	while (i < 6)
 	{
--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,6 +6,7 @@
  * main - prints the result
  * @argc: count of argument
  * @argv: array of argument
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
@@ -18,12 +19,12 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	
+
 	one = atoi(argv[1]);
 	two = atoi(argv[3]);
 	ope = argv[2];
 
-	if (get_op_func(argv[2]) == NULL)
+	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
