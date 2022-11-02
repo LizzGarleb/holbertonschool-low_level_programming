aacf5755169c80407f726520ec9443e180a6c51b
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-02 03:05:51 +0000

    .

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	two = atoi(argv[3]);
 	ope = argv[2];
 
-	if (get_op_func(argv[2]) == NULL) || (argv[2][1] != '\0')
+	if (ope != '*' && ope != '+' && ope != '-' && ope != '/') || (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
