2981a38b39c1b0c4e3c78582b8800d226a30d87e
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-02 03:26:31 +0000

    task 3 lol

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -13,32 +13,26 @@ int main(int argc, char *argv[])
 	int one, two, a;
 	int (*p)(int, int);
 	char *ope;
-
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
 	one = atoi(argv[1]);
 	two = atoi(argv[3]);
 	ope = argv[2];
-
-	if ((argv[2][0] != '*' && argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/') || argv[2][1] != '\0')
+	if ((argv[2][0] != '*' && argv[2][0] != '+' && argv[2][0] != '-' && argv[2][0] != '/' && argv[2][0] != "%") || argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
 	if ((argv[2][0] == '/' && two == 0) || (argv[2][0] == '%' && two == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-
 	p = get_op_func(ope);
 	a = (*p)(one, two);
-
 	printf("%d\n", a);
 	return (0);
 }
