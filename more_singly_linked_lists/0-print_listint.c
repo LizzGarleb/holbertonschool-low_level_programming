3fbfe6cebed79eda52cefea30ba449b2f86196da
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-14 19:08:18 +0000

    task 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - print all elements of a list
+ * @h: point to the start of the linked list
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		printf("%u\n", h->n);
+		h = h->next;
+		count += 1;
+	}
+	return (count);
+}
