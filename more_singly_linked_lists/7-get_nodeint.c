7d30061c078bba527167069fa00405a127132a63
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-15 19:49:17 +0000

    task 7

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - function that returns the nth mode of list
+ * @head: point to the start
+ * @index: is the index of the node, starting at 0
+ * Return: return null if node does not exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *tmp;
+	unsigned int i = 0, j = 0;
+
+	tmp = head;
+	while (tmp != NULL)
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (index > i)
+		return (NULL);
+	else
+	{
+		tmp = head;
+		while (j != index)
+		{
+			tmp = tmp->next;
+			j++;
+		}
+		return (tmp);
+	}
+}
