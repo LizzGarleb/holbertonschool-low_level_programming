a3a85c872a43ac42a15d84541e4aa0f95fc40c8e
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-16 16:58:40 +0000

    putchar function added

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -8,4 +8,7 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+
+/* Helper function */
+int _putchar(char c);
 #endif
