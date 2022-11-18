addd2c3593b73e2eff0c3861c4c299d37c4ad763
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-18 22:22:59 +0000

    correctio of prototype on header

--- file_io/main.h
+++ file_io/main.h
@@ -14,5 +14,5 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 /* Helping function */
-int validating_close(int fd1, int fd2);
+void validating_close(int fd1, int fd2);
 #endif
