ad04bb6493f0243b918aaf7752f7e50656ceab8e
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-22 14:09:21 +0000

    task 8 correction of betty

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -14,27 +14,27 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (*head == NULL)
 		return (-1);
 
-	current_node = *head;
+	prev_node = *head;
 	if (index == 0)
 	{
-		*head = current_node->next;
-		if (current_node->next != NULL)
-			current_node->next->prev = NULL;
-		free(current_node);
+		*head = prev_node->next;
+		if (prev_node->next != NULL)
+			prev_node->next->prev = NULL;
+		free(prev_node);
 		return (1);
 	}
-	while (current_node != NULL && i < index -1)
+	while (prev_node != NULL && i < index - 1)
 	{
-		if (current_node == NULL || current_node->next == NULL)
+		if (prev_node == NULL || prev_node->next == NULL)
 			return (-1);
 
-		current_node = current_node->next;
+		prev_node = prev_node->next;
 		i++;
 	}
-	prev_node = current_node->next->next;
-	if (current_node->next->next != NULL)
-		current_node->next->next->prev = current_node;
-	free(current_node->next);
-	current_node->next = prev_node;
+	current_node = prev_node->next->next;
+	if (prev_node->next->next != NULL)
+		prev_node->next->next->prev = prev_node;
+	free(prev_node->next);
+	prev_node->next = current_node;
 	return (1);
 }
