9bb3b2144d2e9e88bc7533f231160c95dd7ad91d
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-23 20:55:02 +0000

    task 4

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,4 +10,19 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *tmp;
+
+	if (!ht || !key)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp)
+	{
+		if (strcmp(tmp->key,key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
 }
