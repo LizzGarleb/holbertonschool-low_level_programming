6c5d80417639d9bcd73b99461bdc92b832af981a
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-23 18:43:58 +0000

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -10,4 +10,31 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *node = malloc(sizeof(hash_node_t));
+
+	if (key == NULLi || node == NULL || ht == NULL)
+		return (0);
+
+	index = key_index((unsigned char *) key, ht->size);
+
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node->key);
+		return (0);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->value);
+		return (0);
+	}
+
+	if (ht->array[index] == NULL)
+		ht->array[index] = node;
+	else if (strcmp(node->key, key) == 0)
+		strcpy(ht->array[index]->value, value);
+	
+	return (1);
 }
