10aba02c14b944674143ecf6695fd7d6f940375f
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-23 12:32:35 +0000

    task 1 betty

--- hash_tables/1-djb2.c
+++ hash_tables/1-djb2.c
@@ -1,6 +1,8 @@
 #include "hash_tables.h"
 /**
- *
+ * hash_djb2 - A hash function implementing the djb2 algorithm
+ * @str: string used to generate hash value
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
