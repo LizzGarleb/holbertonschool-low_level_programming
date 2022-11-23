a32c0c49d25c079004ea135480216e7d04891f25
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-23 14:49:44 +0000

    task 0

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,12 +10,14 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *table = malloc (sizeof(hash_table_t));
+
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
 	if (table == NULL)
 		return (NULL);
 
 	table->size = size;
-	table->array = malloc (sizeof(hash_node_t *) * table->size);
+	table->array = malloc(sizeof(hash_node_t *) * table->size);
 
 	for (i = 0; i < table->size; i++)
 		table->array[i] = NULL;
