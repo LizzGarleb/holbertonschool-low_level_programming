c6d5efda6d8cac0340f25079b84aec3060a02ab2
Lizz3108 <lizbethgarcialebron@gmail.com>
2022-11-24 13:27:54 +0000

    task 5

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -6,4 +6,44 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i, j;
+	const hash_table_t *index;
+	hash_node_t *tmp;
+
+	if (ht == NULL)
+		return;
+
+	index = ht;
+
+	for (i = 0, j = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			index->array[j] = ht->array[i];
+			j++;
+		}
+	}
+	for (i = 0; i < j; i++)
+	{
+		tmp = index->array[i];
+		if (i == 0)
+			printf("{");
+		if (index->array[i]->next == NULL)
+		{
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			if (i < j - 1)
+				printf(", ");
+			else
+				printf("}\n");
+		}
+		if (index->array[i]->next != NULL)
+		{
+			while (tmp)
+			{
+				printf("'%s': '%s'", tmp->key, tmp->value);
+				printf(", ");
+				tmp = tmp->next;
+			}
+		}
+	}
 }
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+#include <stdio.h>
 
 /* Structures */
 /**
